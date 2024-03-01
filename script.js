ea371d61d9e4ed11b3f4a81b337c828d3be501e0
Francesca Falconi <falconi.francesca@libero.it>
2024-03-01 16:20:20 +0100

    cambio colori

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-$(document).ready(function() {
-    $(".interruttore").click(function() {
-        if ($(".description").is(":visible")) {
-            $(".description").fadeOut(500, "linear");
-        } else {
-            $(".description").fadeIn(500, "linear");
-        }
-    });
+$(".interruttore").click(function() {
+    if ($(".description").is(":visible")) {
+        $(".description").fadeOut(500, "linear");
+    } else {
+        $(".description").fadeIn(500, "linear");
+    }
 });
+
